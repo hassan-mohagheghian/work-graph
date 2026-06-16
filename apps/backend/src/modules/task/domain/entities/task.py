8d3762c77eb55d1942c525cf74290fffc3c12a9d
Hassan Mohagheghian <hassan.mohagheghian.cs@gmail.com>
2026-06-16 18:45:55 +0330

    feat(back/task): change status in model in follow entity

--- apps/backend/src/modules/task/domain/entities/task.py
+++ apps/backend/src/modules/task/domain/entities/task.py
@@ -3,6 +3,7 @@ from datetime import datetime, timezone
 from uuid import UUID
 
 from src.modules.shared.domain.entity_v1 import Entity
+from src.modules.task.domain.value_objects.task_status import TaskStatus
 
 
 @dataclass(eq=False)
@@ -11,5 +12,12 @@ class Task(Entity):
     org_id: UUID
     title: str
     description: str | None = None
-    status: str = "todo"
+    status: TaskStatus = TaskStatus.TODO
     created_at: datetime = field(default_factory=lambda: datetime.now(timezone.utc))
+
+
+ALLOWED_TRANSITIONS = {
+    TaskStatus.TODO: {TaskStatus.IN_PROGRESS},
+    TaskStatus.IN_PROGRESS: {TaskStatus.DONE},
+    TaskStatus.DONE: set(),
+}
--- apps/backend/src/modules/task/infrastructure/persistence/models.py
+++ apps/backend/src/modules/task/infrastructure/persistence/models.py
@@ -1,8 +1,10 @@
 import uuid
+from enum import Enum
 
 from sqlalchemy import Column, String
 from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 from sqlalchemy.orm import Mapped, mapped_column
+from src.modules.task.domain.value_objects.task_status import TaskStatus
 from src.modules.task.infrastructure.persistence.base import Base
 from src.shared.infrastructure.persistence.base import CreateAtMixin, IDMixin
 
@@ -15,4 +17,6 @@ class TaskModel(IDMixin, CreateAtMixin, Base):
     org_id = Column(PG_UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     title = Column(String(200), unique=True, nullable=False)
     description: Mapped[str | None]
-    status: Mapped[str] = mapped_column(String(50), default="todo")
+    status: Mapped[str] = mapped_column(
+        Enum(TaskStatus, name="task_status"), default=TaskStatus.TODO
+    )
